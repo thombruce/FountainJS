65ce8f3ff4af6c918221f87a9aaadade080e37c6
Thom Bruce <thom@thombruce.com>
2021-05-22 23:54:50 +0100

    Use render function to allow usage with slot

--- src/components/Screenplay.vue
+++ src/components/Screenplay.vue
@@ -1,18 +1,52 @@
-<template>
-  <article class='screenplay'>
-    <header class='title-page' v-if='title' v-html='screenplay.html.title_page'></header>
-    <div class='script' v-html='screenplay.html.script'></div>
-  </article>
-</template>
-
 <script>
+import fountain from '../fountain'
+
+var getChildrenTextContent = function (children) {
+  return children.map(function (node) {
+    return node.children
+      ? getChildrenTextContent(node.children)
+      : node.text
+  }).join('\n')
+}
+
 export default {
   props: {
-    screenplay: Object,
+    screenplay: [Object, String],
     title: {
       type: Boolean,
       default: false
     }
+  },
+  computed: {
+    document () {
+      if (this.$slots.default) return fountain.parse(getChildrenTextContent(this.$slots.default))
+      if (typeof this.screenplay === 'string') return fountain.parse(this.screenplay)
+      if (typeof this.screenplay === 'object' && this.screenplay.html && this.screenplay.html.script) return this.screenplay
+      return false
+    }
+  },
+  render: function (createElement) {
+    if (this.document) {
+      const titlePage = createElement(
+        'header',
+        { attrs: { class: 'title-page' }, domProps: { innerHTML: this.document.html.title_page } }
+      )
+
+      const script = createElement(
+        'div',
+        { attrs: { class: 'script' }, domProps: { innerHTML: this.document.html.script } }
+      )
+
+      return createElement(
+        'article',
+        { attrs: { class: 'screenplay' } },
+        [
+          ...(this.title ? [titlePage] : []),
+          script
+        ]
+      )
+    }
+    return createElement('article', { attrs: { class: 'screenplay' } })
   }
 }
 </script>
