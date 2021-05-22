7203c74c1ea766b839d18b633ecdaf3bdf9570ca
Thom Bruce <thom@thombruce.com>
2021-05-22 20:11:16 +0100

    Initial commit

---
+++ src/components/Screenplay.vue
@@ -0,0 +1,22 @@
+<template>
+  <article class='screenplay'>
+    <header class='title-page' v-if='title' v-html='document.html.title_page'></header>
+    <div class='script' v-html='document.html.script'></div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    screenplay: Object,
+    title: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style>
+@import '../assets/css/screenplay';
+</style>
