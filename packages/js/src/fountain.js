c680b61c9671d2c3b7de33b8195cece7d2b9c3fa
Thom Bruce <thom@thombruce.com>
2024-07-21 05:14:35 +0100

    add parsing for lyric dialogue lines

--- packages/js/src/fountain.js
+++ packages/js/src/fountain.js
@@ -12,6 +12,8 @@
     dialogue: /^([A-Z*_]+[0-9A-Z (._\-')]*)(\^?)?(?:\n(?!\n+))([\s\S]+)/,
     parenthetical: /^(\(.+\))$/,
 
+    lyric: /^~(.+?)$/gm,
+
     action: /^(.+)/g,
     centered: /^(?:> *)(.+)(?: *<)(\n.+)*/g,
         
@@ -166,6 +168,8 @@
   };
 
   var inline = {
+    lyric: '<span class=\"lyric\">$1</span>',
+
     note: '<!-- $1 -->',
 
     line_break: '<br />',
@@ -176,7 +180,7 @@
     bold_italic: '<span class=\"bold italic\">$2</span>',
     bold: '<span class=\"bold\">$2</span>',
     italic: '<span class=\"italic\">$2</span>',
-    underline: '<span class=\"underline\">$2</span>'
+    underline: '<span class=\"underline\">$2</span>',
   };
 
   inline.lexer = function (s) {
@@ -187,7 +191,12 @@
     var styles = [ 'underline', 'italic', 'bold', 'bold_italic', 'italic_underline', 'bold_underline', 'bold_italic_underline' ]
            , i = styles.length, style, match;
 
-    s = s.replace(regex.note_inline, inline.note).replace(/\\\*/g, '[star]').replace(/\\_/g, '[underline]').replace(/\n/g, inline.line_break);
+    s = s
+      .replace(regex.lyric, inline.lyric)
+      .replace(regex.note_inline, inline.note)
+      .replace(/\\\*/g, '[star]')
+      .replace(/\\_/g, '[underline]')
+      .replace(/\n/g, inline.line_break);
 
    // if (regex.emphasis.test(s)) {                         // this was causing only every other occurence of an emphasis syntax to be parsed
       while (i--) {
